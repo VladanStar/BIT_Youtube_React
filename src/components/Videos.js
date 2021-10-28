cd9dd915d8c4c1b18cf6bd4da3f664fe0cc5b7cf
unknown <vladancupric@gmail.com>
2021-10-28 11:14:06 +0200

    New commits videos

---
+++ src/components/Videos.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./Video.css";
+
+const Video = (props) => {
+    return (
+        <iframe
+                width={props.width}
+                height={props.height}
+                src={`https://www.youtube.com/embed/${props.chosenVideo}`}
+                frameBorder="0"
+                title={`Video_${props.chosenVideo}`}
+                gesture="media"
+                allow="encrypted-media"
+                allowFullScreen
+                className="Video_iframe">
+        </iframe>
+    );
+}
+
+export default Video;
